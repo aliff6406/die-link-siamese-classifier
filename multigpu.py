d5143b6b480331d3b0614043b2ede594037263ab
Aliff Amir <aliff6406@gmail.com@users.noreply.github.com>
2024-02-21 15:08:41 +0000

    latest commit

--- multigpu.py
+++ multigpu.py
@@ -5,20 +5,25 @@ from pytz import timezone
 
 import torch
 import torch.nn as nn
-from torch.utils.data import DataLoader
-from torchvision import transforms
-import torch.nn.functional as F
-from torch.utils.tensorboard import SummaryWriter
-import torch.distributed as dist
+from torch.utils.data import DataLoader, Dataset
+import torch.multiprocessing as mp
+from torch.utils.data.distributed import DistributedSampler
 from torch.nn.parallel import DistributedDataParallel as DDP
-from torch.utils.data import DataLoader, DistributedSampler
+from torch.distributed import init_process_group, destroy_process_group
 
 import matplotlib.pyplot as plt
 
-import config
-from tensorsiamese import SiameseNetworkSAM
-from contrastive import ContrastiveLoss
-from tensorpair_v1 import SiameseTensorPairDataset
+def ddp_setup(rank, world_size):
+    """
+    Function from https://github.com/pytorch/examples/blob/main/distributed/ddp-tutorial-series/multigpu.py
+    Args:
+        rank: Unique identifier of each process
+        world_size: Total number of processes
+    """
+    os.environ["MASTER_ADDR"] = "localhost"
+    os.environ["MASTER_PORT"] = "12355"
+    init_process_group(backend="nccl", rank=rank, world_size=world_size)
+    torch.cuda.set_device(rank)
 
 def cur_time():
     fmt = '%Y-%m-%d %H:%M:%S %Z%z'
@@ -44,174 +49,107 @@ def plot_accuracy(train_accs, val_accs, out_path):
     plt.savefig(os.path.join(out_path, 'accuracy_plot.jpg'))
     plt.close()
 
-def ddp_setup(rank, world_size):
-    os.environ['MASTER_ADDR'] = 'localhost'
-    os.environ['MASTER_PORT'] = '12355'
-    dist.init_process_group("nccl", rank=rank, world_size=world_size)
-    torch.cuda.set_device(rank)
+class Trainer:
+    def __init__(
+        self,
+        model: torch.nn.Module,
+        train_data: DataLoader,
+        val_data: DataLoader,
+        optimizer: torch.optim.Optimizer,
+        gpu_id: int,
+        save_every: int
+        ) -> None:
+            self.gpu_id = gpu_id
+            self.model = model.to(gpu_id)
+            self.train_data = train_data
+            self.optimizer = optimizer
+            self.save_every = save_every
+            self.model = DDP(model, device_ids=[gpu_id])
+            self.criterion = nn.BCELoss()
 
-def train_samsiamese():
+            self.epoch_train_losses = []
+            self.epoch_val_losses = []
+            self.train_losses = []
+            self.train_accs = []
+            self.val_losses = []
+            self.val_accs = []
+
+            self.best_val_loss = 100000000
+
+    def _train_batch(self, source1, source2, targets):
+        self.optimizer.zero_grad()
+        output = self.model(source1, source2)
+        loss = self.criterion(output, targets)
+        loss.backward()
+        self.optimizer.step()
+
+    def _val_batch(self, source1, source2, targets):
+        with torch.no_grad():
+            output = self.model(source1, source2)
+            loss = self.criterion(output, targets)
+
+    def _run_epoch(self, epoch):
+        b_sz = len(next(iter(self.train_data))[0])
+        print(f"[GPU{self.gpu_id}] Epoch {epoch} | Batchsize: {b_sz} | Steps: {len(self.train_data)}")
+        self.train_data.sampler.set_epoch(epoch)
+
+
+        self.model.train()
+        for (source1, source2), targets in self.train_data:
+            source1, source2, targets = map(lambda x: x.to(self.gpu_id), [source1, source2, targets])
+            self._train_batch(source1, source2, targets)
+
+        self.model.eval()
+        for (source1, source2), targets in self.val_data:
+            source1, source2, targets = map(lambda x: x.to(self.gpu_id), [source1, source2, targets])
+            self._val_batch(source1, source2, targets)
+
+    def _save_checkpoint(self, epoch):
+        ckp = self.model.module.state_dict()
+        PATH = "checkpoint.pt"
+        torch.save(ckp, PATH)
+        print(f"Epoch {epoch} | Training checkpoint saved at {PATH}")
+
+    def train(self, max_epochs: int):
+        for epoch in range(max_epochs):
+            self._run_epoch(epoch)
+            if self.gpu_id == 0 and epoch % self.save_every == 0:
+                self._save_checkpoint(epoch)
+
+def load_train_objs():
+    # train_set =   # load your dataset
+    # val_set = 
+    # model =   # load your model
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
+    return train_set, model, optimizer
+
+
+def prepare_dataloader(dataset: Dataset, batch_size: int):
+    return DataLoader(
+        dataset,
+        batch_size=batch_size,
+        pin_memory=True,
+        shuffle=False,
+        sampler=DistributedSampler(dataset)
+    )
+
+
+def main(rank: int, world_size: int, save_every: int, total_epochs: int, batch_size: int):
     ddp_setup(rank, world_size)
+    dataset, model, optimizer = load_train_objs()
+    train_data = prepare_dataloader(dataset, batch_size)
+    trainer = Trainer(model, train_data, optimizer, rank, save_every)
+    trainer.train(total_epochs)
+    destroy_process_group()
 
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    # Directory Config
-    train_dir = config.obverse_train_dir
-    train_csv = config.obverse_train_csv
-    val_dir = config.obverse_validate_dir
-    val_csv = config.obverse_validate_csv
-
-    runs_dir = config.output_path
-
-    # Create Directory to Store Experiment Artifacts
-    artifact_dir_name = cur_time()
-    artifact_path = os.path.join(runs_dir, artifact_dir_name)
-    os.makedirs(artifact_path)
-
-    # Hyperparameters
-    batch_size = 1
-    num_epochs = 100
-    learning_rate = 0.001
-
-    # Training Settings - later to be implemented with ArgumentParser()
-    contra_loss = None
-    contra_margin = 1
-
-    sam_transform = transforms.Compose([
-        transforms.PILToTensor(), # Convert to tensor from PIL image
-        transforms.Resize(size=(1024,1024), antialias=False) # Resize to 1024x1024
-    ])
-
-    train_dataset = SiameseTensorPairDataset(label_dir=train_csv, tensor_dir=train_dir)
-    val_dataset = SiameseTensorPairDataset(label_dir=val_csv, tensor_dir=val_dir)
-
-    # Default batch_size = 1 if not set
-    train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-    val_dataloader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
-
-    samsiamese_model = SiameseNetworkSAM(contrastive_loss=contra_loss)
-
-    if torch.cuda.device_count() > 1:
-        samsiamese_model = nn.DataParallel(samsiamese_model)
-
-    samsiamese_model.to(device)
-
-    # Initialise Loss Function
-    if contra_loss:
-        criterion = ContrastiveLoss(margin=contra_margin)
-    else:
-        criterion = nn.BCELoss()
-
-    # Initialise Optimizer - can experiment with different optimizers
-    # Here we use Adam  
-    optimizer = torch.optim.Adam(samsiamese_model.parameters(), lr=learning_rate)
-
-    writer = SummaryWriter(log_dir=artifact_path)
-
-    best_val_loss = 100000000
-
-    for epoch in range(num_epochs):
-        print("Epoch [{} / {}]".format(epoch+1, num_epochs))
-        epoch_start = time.time()
-        samsiamese_model.train()
-
-        losses = []
-        train_losses = []
-        train_accs = []
-        correct_pred = 0
-        total_pred = 0
-
-        # Training Loop Start
-        for (tensor1, tensor2), label in train_dataloader:
-            tensor1, tensor2, label = map(lambda x: x.to(device), [tensor1, tensor2, label])
-            optimizer.zero_grad()
-            if contra_loss:
-                output1, output2 = samsiamese_model(tensor1, tensor2)
-                loss = criterion(output1, output2, label)
-                loss.backwards()
-                optimizer.step()
-
-                losses.append(loss.item())
-                correct_pred += torch.count_nonzero(label == (F.pairwise_distance(output1, output2) < contra_margin)).item()
-                total_pred += len(label)
-            else:
-                prob = samsiamese_model(tensor1, tensor2)
-                loss = criterion(prob, label.view(-1))
-                loss.backward()
-                optimizer.step()
-
-                losses.append(loss.item())
-                correct_pred += torch.count_nonzero(label == (prob > 0.5)).item()
-                total_pred += len(label)
-
-        avg_train_loss = sum(losses) / len(losses)
-        avg_train_acc = correct_pred / total_pred
-
-        train_losses.append(avg_train_loss)
-        train_accs.append(avg_train_acc)
-
-        writer.add_scalar("train_loss", avg_train_loss, epoch+1)
-        writer.add_scalar("train_accuracy", avg_train_acc, epoch+1)
-
-        print("Epoch [{} / {}] | Training: Loss={:.2f} | Accuracy={:.2f}".format(epoch+1, num_epochs, sum(train_losses)/len(train_losses), correct_pred / total_pred))
-        # Training Loop End
-
-        # Validation Loop Start
-        samsiamese_model.eval()
-
-        losses = []
-        val_losses = []
-        val_accs = []
-        correct_pred = 0
-        total_pred = 0
-
-        for (tensor1, tensor2), label in val_dataloader:
-            tensor1, tensor2, label = map(lambda x: x.to(device), [tensor1, tensor2, label])
-
-            if contra_loss:
-                output1, output2 = samsiamese_model(tensor1, tensor2)
-                loss = criterion(output1, output2, label)
-
-                losses.append(loss.item())
-                correct_pred += torch.count_nonzero(label == (F.pairwise_distance(output1, output2) < contra_margin)).item()
-                total_pred += len(label)
-            else:
-                prob = samsiamese_model(tensor1, tensor2)
-                loss = criterion(prob, label.view(-1))
-
-                losses.append(loss.item())
-                correct_pred += torch.count_nonzero(label == (prob > 0.5)).item()
-                total_pred += len(label)
-            
-        val_loss = sum(losses) / max(1, len(losses))
-        avg_val_acc = correct_pred / total_pred
-
-        val_losses.append(val_loss)
-        val_accs.append(avg_val_acc)
-
-        writer.add_scalar("val_loss", val_loss, epoch+1)
-        writer.add_scalar("val_acc", correct_pred / total_pred, epoch+1)
-
-        epoch_end = time.time()
-        epoch_time = epoch_end - epoch_start
-
-        print("Epoch [{} / {}]: Time={:.2f} | Validation: Loss={:.2f} | Accuracy={:.2f}".format(epoch+1, num_epochs, epoch_time, val_loss, correct_pred / total_pred))
-        # Validation Loop End
-
-        # Update "best.pt" model if val_loss of current epoch is lower than the best validation loss
-        if val_loss < best_val_loss:
-            best_val_loss = val_loss
-            torch.save(
-                {
-                    "epoch": epoch + 1,
-                    "model_state_dict": samsiamese_model.state_dict(),
-                    "optimizer_state_dict": optimizer.state_dict()
-                },
-                os.path.join(artifact_path, "best.pt")
-            )
-    
-    # Plot Loss and Accuracy 
-    plot_loss(train_losses, val_losses, artifact_path)
-    plot_accuracy(train_accs, val_accs, artifact_path)
 
 if __name__ == "__main__":
-    train_samsiamese()
+    import argparse
+    parser = argparse.ArgumentParser(description='simple distributed training job')
+    parser.add_argument('total_epochs', type=int, help='Total epochs to train the model')
+    parser.add_argument('save_every', type=int, help='How often to save a snapshot')
+    parser.add_argument('--batch_size', default=32, type=int, help='Input batch size on each device (default: 32)')
+    args = parser.parse_args()
+    
+    world_size = torch.cuda.device_count()
+    mp.spawn(main, args=(world_size, args.save_every, args.total_epochs, args.batch_size), nprocs=world_size)
