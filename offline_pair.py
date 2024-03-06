1659a312422d5671ce16613c6cbb2b9494a8f8f5
Aliff Amir <aliff6406@gmail.com@users.noreply.github.com>
2024-03-06 01:22:09 +0000

    feat: add offline pairing

---
+++ offline_pair.py
@@ -0,0 +1,25 @@
+import os
+
+import pandas as pd
+import torch
+from torch.utils.data import Dataset
+
+class OfflinePairDataset(Dataset):
+    def __init__(self, pair_dir, tensor_dir, shuffle_pairs=True):
+        self.pair_df = pd.read_csv(pair_dir, header=None)
+        self.tensor_dir = tensor_dir
+        self.shuffle_pairs = shuffle_pairs
+
+    def __len__(self):
+        return len(self.pair_df)
+    
+    def __getitem__(self, idx):
+        tensor1_path = os.path.join(self.tensor_dir, self.pair_df.iloc[idx, 0])
+        tensor2_path = os.path.join(self.tensor_dir, self.pair_df.iloc[idx, 1])
+        label = self.pair_df[idx, 2]
+
+        tensor1 = torch.load(tensor1_path, map_location='cpu')
+        tensor2 = torch.load(tensor2_path, map_location='cpu')
+
+        # Return the tensor pair and the label (1 for positive pair, 0 for negative pair)
+        return (tensor1, tensor2), torch.tensor(label, dtype=torch.float32)
